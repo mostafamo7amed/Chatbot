23ebaa762b43a29cc9bd74378f54bab3a522bd1a
Moustafa Mohamed <moustafamohmed203@gmail.com>
2024-01-11 20:54:30 +0200

    add constant

---
+++ lib/chat/constant.dart
@@ -0,0 +1,4 @@
+class Constant {
+  final base_uri = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?';
+  final api_key = 'key=AIzaSyA54Cok3T4YyDbEFrwF698xwrnJRd5B4Dk';
+}
