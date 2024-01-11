9f7557c84df80dc6101e9bc7bd1e01b851708cfe
Moustafa Mohamed <moustafamohmed203@gmail.com>
2024-01-11 20:45:24 +0200

    upload project

---
+++ lib/chat/chatbot.dart
@@ -0,0 +1,68 @@
+import 'dart:convert';
+import 'package:chatbot/chat/constant.dart';
+import 'package:dash_chat_2/dash_chat_2.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class ChatView extends StatefulWidget {
+  const ChatView({Key? key}) : super(key: key);
+
+  @override
+  State<ChatView> createState() => _ChatViewState();
+}
+
+class _ChatViewState extends State<ChatView> {
+  ChatUser human = ChatUser(id: '1', firstName: 'Mostafa', lastName: 'Mohamed');
+  ChatUser bot = ChatUser(id: '2', firstName: 'Gemini');
+  final url = Constant().base_uri + Constant().api_key;
+  List<ChatMessage> allMessages = [];
+  List<ChatUser> typing = [];
+
+  final header = {'Content-Type': 'application/json'};
+
+  getMessages(ChatMessage m) async {
+    allMessages.insert(0, m);
+    typing.add(bot);
+    setState(() {});
+    var data = {
+      "contents": [
+        {
+          "parts": [
+            {"text": m.text}
+          ]
+        }
+      ]
+    };
+    await http
+        .post(Uri.parse(url), headers: header, body: jsonEncode(data))
+        .then((value) {
+      if (value.statusCode == 200) {
+        var response = jsonDecode(value.body);
+        ChatMessage m2 = ChatMessage(
+          user: bot,
+          text: response['candidates'][0]['content']['parts'][0]['text'],
+          createdAt: DateTime.now(),
+        );
+        allMessages.insert(0, m2);
+      } else {
+        print('error');
+      }
+    }).catchError((e) {});
+    typing.remove(bot);
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: DashChat(
+        typingUsers: typing,
+        currentUser: human,
+        onSend: (ChatMessage message) {
+          getMessages(message);
+        },
+        messages: allMessages,
+      ),
+    );
+  }
+}
